34f5afd533b72ab73c6b0460748e051b5e72eaa0
mjones-auth0 <76823355+mjones-auth0@users.noreply.github.com>
2021-01-06 08:52:19 -0600

    Update index.js

--- index.js
+++ index.js
@@ -38,12 +38,11 @@ app.use(
    authRequired: false,
    auth0Logout: true,
    baseURL: APP_URL,
-   // 👇 add this 👇
    authorizationParams: {
      response_type: "code id_token",
      audience: "https://expenses-api",
+     scope: "openid profile email read:reports",
    },
-   // 👆 add this 👆
  })
 );
 
